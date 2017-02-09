b5cff5ee46088643355ff8ebec880f903341f044
Pedro Vanzella <pedro@pedrovanzella.com>
2017-02-09 21:43:29 -0200

    Drawing a triangle

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -81,15 +81,20 @@ int main(int argc, char * argv[]) {
      0.0f,  0.5f, 0.0f
     };
 
-    GLuint VBO;
+    GLuint VBO, VAO;
     glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glGenVertexArrays(1, &VAO);
 
+    /* -------- Initialization -------- */
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glUseProgram(shaderProgram);
+
+    glBindVertexArray(0); // Unbind the VAO
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
@@ -98,6 +103,11 @@ int main(int argc, char * argv[]) {
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+    
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
